77c42b710f3c0df87cca42a4e093111eec3ff6a7
Nseudouh <5udoun19@solent.ac.uk>
2022-08-26 11:46:46 +0100

    streamlit ongoint

---
+++ testing.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+import numpy as np
+import re
+import transformers
+from transformers import pipeline
+
+
+st.title("Mobile Telecommunication Sentiment Analysis")
+st.markdown("This application is all about sentiment analysis of network providers. Analysising the reviews of custoners through the use of streamlit.")
+st.sidebar.title("Sentiment Analysis of  Network Providers")
+st.sidebar.markdown('Displaying the analysis of passengers reviews and the deploying the models that was implemented')
+
+input_text = st.text_input("Get Predictions", value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False)
+if input_text:
+    sentiment_pipe = pipeline("sentiment-analysis")
+    res = sentiment_pipe(input_text)
+    #print(res)
+    st.write("Your predict is ", "POSITIVE")
+
+data= pd.read_csv("Reviews Sentiment.csv")
+
+if st.checkbox("Show new_data"):
+    st.write(data.head(50))
